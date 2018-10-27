1413e8b373421ac33bcb44d17a7cb547229e6093
ryanyun2010 <ryanyun2010@gmail.com>
2018-10-27 13:42:46 -0700

    commit

--- script.js
+++ script.js
@@ -156,9 +156,9 @@ function chkWinner(bd_wrong) {
             if (chkLine(bd[r][c], bd[r-1][c+1], bd[r-2][c+2], bd[r-3][c+3])){
 							draw.fillStyle="#ffff00"
 							draw.fillRect(r*60-180,350+c*50,60,50)
-							draw.fillRect((r-1)*60-120,350+(c+1)*50,60,50)
-							draw.fillRect((r-2)*60-120,350+(c+2)*50,60,50)
-							draw.fillRect((r-3)*60-120,350+(c+3)*50,60,50)
+							draw.fillRect((r-1)*60-180,350+(c+1)*50,60,50)
+							draw.fillRect((r-2)*60-180,350+(c+2)*50,60,50)
+							draw.fillRect((r-3)*60-180,350+(c+3)*50,60,50)
 							draw.stroke()
 							return bd[r][c];
 
